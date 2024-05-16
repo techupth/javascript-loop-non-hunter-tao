99b7b9aeb7289c2479e964f14dfd355aedf8b103
Hunter <hunvicter@gmail.com>
2024-05-16 15:10:06 +0700

    hunter/solved probs

--- ex-4.js
+++ ex-4.js
@@ -2,5 +2,10 @@
 let restaurants = ["KFC", "MOMO PARADISE", "Nabezo"];
 let newRestaurants = [];
 // Start coding here
-
+for(let name of restaurants){
+    newRestaurants.push (`Restaurant Name: ${name}`)
+}
 console.log(newRestaurants);
+
+
+
--- ex-6.js
+++ ex-6.js
@@ -1,7 +1,9 @@
-// Exercise #6: Reverse the String
 let companyName = "TechUp Thailand";
 let reversedCompanyName = "";
 
-// Start coding here
+for (let i = companyName.length-1;i>=0; i--) {
+    console.log(companyName[i])
+    reversedCompanyName = reversedCompanyName + companyName[i];
+}
 
 console.log(reversedCompanyName);
